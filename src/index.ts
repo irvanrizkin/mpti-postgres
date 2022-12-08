7bb5ad0738ae02c0d9f54f252061e0655bec7bcd
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2022-12-08 18:09:10 +0700

    env

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 import bodyParser from "body-parser";
 import express from "express";
 import pg from "pg";
+import { Sequelize } from "sequelize";
 
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
-const pool = new pg.Pool();
+// const pool = new pg.Pool();
+const sequelize = new Sequelize();
+console.log(process.env);
+
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(express.json());
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
-});
+// app.get("/", async (req, res) => {
+//   const { rows } = await pool.query("SELECT NOW()");
+//   res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
